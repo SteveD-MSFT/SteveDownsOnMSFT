5532804e20af5aa52b073c3e065ee8807818a795
Steve Downs <steve.downs@microsoft.com>
2024-01-09 12:59:59 -0500

    TEst

--- maintenance/register/prepad.ps1
+++ maintenance/register/prepad.ps1
@@ -5,4 +5,13 @@ $DomainFQDN = 'lab.windowsinthe.cloud'
 $ashciClusterName = 'VAHCICLUSTER01'
 $AsHciDeploymentPrefix = 'VAHCI'
 
+# Download the module from GitHub
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/HCI/AsHciADArtifactsPreCreationTool.psm1" -OutFile "C:\temp\AsHciADArtifactsPreCreationTool.psm1"
+
+# Import the module
+Import-Module "C:\temp\AsHciADArtifactsPreCreationTool.psm1"
+
+# Install the module
+Install-Module -Name AsHciADArtifactsPreCreationTool
+
 New-HciAdObjectsPreCreation -Deploy -AzureStackLCMUserCredential $HCIcredential -AsHciOUName $AsHciOUName -AsHciPhysicalNodeList $AsHciPhysicalNodeList -DomainFQDN $DomainFQDN -AsHciClusterName $ashciClusterName -AsHciDeploymentPrefix $AsHciDeploymentPrefix
